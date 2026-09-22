9a472b5116e6718eaa82cedb254367232b687213
seanpasion8 <seanandrei.pasion@antipolo.benilde.edu.ph>
2026-09-22 05:50:39 +0000

    webpage

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENTPROGasp.net.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
